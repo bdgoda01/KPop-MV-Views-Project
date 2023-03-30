ae787b8ac255caa9bb938db8b66216a8d63fc415
bdgoda01 <binugodage@gmail.com>
2023-03-30 18:49:51 -0400

    third commit

--- views_scraper.py
+++ views_scraper.py
@@ -1,11 +1,18 @@
 import pandas as pd
 
 #need to make custom functions to operate on my data
+#
+# 
+# ONLY NEED MORE THAN 1!!!
+#
+#
 #function 1: gets the year w/lowest view increase
 #function 2: gets total views by artist by year
 #function 3: gets the year w/highest view increase
 #function 4: gets the music video w/highest view increase in a year
 #function 5: gets the artist w/highest view increase in a year
+
+#need to make a requirements txt
 
 #need to make plots
 #plot 1: line chart of top 5 most viewed kpop music videos for total views across time (years)
@@ -22,34 +29,24 @@ monthlyViews_df = views_dfs[2]
 yearlyViews_df.rename(columns={"Views":"Views (string)"}, inplace=True)
 monthlyViews_df.rename(columns={"Views":"Views (string)"}, inplace=True)
 
-yearlyViews_df["Views"] = 0
-monthlyViews_df["Views"] = 0
+def yearlyViewCleanup(original_df):
+    yearlyViews_years = []
+    yearlyViews_viewsDiff = []
+    yearlyViews_yearCount = []
+    yearlyViews_viewTotal = []
+    for i in range(len(original_df)):
+        yearlyViews_yearCount.append(i+1)
+        yearlyViews_years.append(original_df.loc[i, "Year"])
+        yrViews_str = original_df.loc[i, "Views (string)"]
+        yrViews_int = int(yrViews_str[1:].replace(',',''))
+        yearlyViews_viewsDiff.append(yrViews_int)
+        if i == 0:
+            yearlyViews_viewTotal.append(yrViews_int)
+        else:
+            yearlyViews_viewTotal.append((yearlyViews_viewTotal[i-1] + yrViews_int))
+    
+    clean_df = pd.DataFrame({"Year": yearlyViews_years, "Approx. Total Views": yearlyViews_viewTotal, "Approx. Views Increase": yearlyViews_viewsDiff}, index=yearlyViews_yearCount)
+    clean_df.rename_axis("Year Count", inplace=True)
+    return clean_df
 
-yearlyViews_years = []
-yearlyViews_viewsDiff = []
-yearlyViews_yearCount = []
-yearlyViews_viewTotal = []
-
-#turn this into a function to meet project requirements!
-
-for i in range(len(yearlyViews_df)):
-    yearlyViews_yearCount.append(i+1)
-    yearlyViews_years.append(yearlyViews_df.loc[i, "Year"])
-    yrViews_str = yearlyViews_df.loc[i, "Views (string)"]
-    yrViews_int = int(yrViews_str[1:].replace(',',''))
-    yearlyViews_viewsDiff.append(yrViews_int)
-    if i == 0:
-        yearlyViews_viewTotal.append(yrViews_int)
-    else:
-        yearlyViews_viewTotal.append((yearlyViews_viewTotal[i-1] + yrViews_int))
-
-
-cleanYearlyViews_df = pd.DataFrame({"Year": yearlyViews_years, "Approx. Total Views": yearlyViews_viewTotal, "Approx. Views Increase": yearlyViews_viewsDiff}, index=yearlyViews_yearCount)
-cleanYearlyViews_df.rename_axis("Year Count", inplace=True)
-print(cleanYearlyViews_df)
-
-""" for i in range(12):
-    mthViews_str = monthlyViews_df.loc[i, "Views (string)"]
-    monthlyViews_df.loc[i, "Views"] = int(mthViews_str[1:].replace(',',''))
-
-print(yearlyViews_df.head()) """
+cleanYearlyViews_df = yearlyViewCleanup(yearlyViews_df)
